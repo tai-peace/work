f67f38c86b7e5a20d24740206fd1948c12bcdaa7
”名前” <sokakaka1125@icloud.com>
2019-10-02 11:00:38 +0000

    課題提出2

---
+++ sum.php
@@ -0,0 +1,25 @@
+ <?php
+ function sum($max){
+  echo $max*2;
+ }
+ 
+    echo sum(6);
+  
+  function pot($a,$b){
+echo $a+$b;
+  }
+  echo pot(2,2);
+  
+  function sun($arr)
+  {$aaray=array(1,3,5,7,9);
+  echo array_product($aaray); 
+  }
+  echo sun(1000);
+  
+$str="<h1>タグの取り除き</h1>";
+echo strip_tags($str) ;dddd
+
+
+?>
+
+  
